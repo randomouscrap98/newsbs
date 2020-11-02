ea69540c84ea4f688065fa338eea436d4d84cf63
Carlos Sanchez <randomouscrap98@aol.com>
2020-11-02 07:11:06 +0000

    Added first new template, working

--- template.js
+++ template.js
@@ -19,22 +19,27 @@ function Template(name, element, fields)
    this.functionPool = {};
 }
 
-var TemplateSystem = Object.create(null); with (TemplateSystem) (function($) { Object.assign(TemplateSystem, 
+Template.prototype.SetFields = function(fields)
+{
+   var me = this;
+   Object.keys(fields).forEach(x => me.fields[x] = fields[x]);
+};
+
+var StdTemplating = Object.create(null); with (StdTemplating) (function($) { Object.assign(StdTemplating, 
 {   
    //Ensure only a single field is ever defined for an object
    SingleField: function(obj, key, define)
    {
-      if(key in tobj.fields)
+      if(key in obj)
          throw "Duplicate field: " + key;
       define.enumerable = true;
       define.configurable = true;
       Object.defineProperty(obj, key, define);
       return obj;
    },
-   //Simple single field: just a value (no getter/setter)
    SingleFieldValue : function(obj, key, value)
    {
-      return SingleField(obj, key, { value : value, writable: true });
+      return SingleField(obj, key, { value: value, writable: true });
    },
    //Read the field then remove it immediately afterwards.
    StripField : function(element, field, onlyExists)
@@ -45,7 +50,7 @@ var TemplateSystem = Object.create(null); with (TemplateSystem) (function($) { O
    },
    //Try to perform replacement on currentelement (if it's a template link,
    //otherwise do nothing)
-   StdTemplateTryReplace : function(currentelement, tobj)
+   TryReplace : function(currentelement, tobj)
    {
       if(!currentelement) return
 
@@ -57,7 +62,7 @@ var TemplateSystem = Object.create(null); with (TemplateSystem) (function($) { O
       if(link)
       {
          //Need to perform standard initialization. Store the template as an inner
-         var innertobj = StdTemplate(link);
+         var innertobj = Load(link, tobj.functionPool);
          SingleFieldValue(tobj.innerTemplates, name, innertobj);
 
          //Replace ourselves with this.
@@ -80,7 +85,7 @@ var TemplateSystem = Object.create(null); with (TemplateSystem) (function($) { O
 
       return link;
    },
-   StdTemplateProcessField: function(currentelement, fieldname, tobj)
+   ProcessField: function(currentelement, fieldname, tobj)
    {
       //Nothing to do!
       if(!fieldname.startsWith("t-"))
@@ -99,71 +104,90 @@ var TemplateSystem = Object.create(null); with (TemplateSystem) (function($) { O
          {
             var func = property.substr(0, func.length - 2);
 
-            if(!func in tobj.functionPool) //currentelement)
+            if(!(func in tobj.functionPool)) //currentelement)
                throw "No function " + func + " in template: " + tobj.name;
 
-            SingleField(tobj.fields, func, {
+            SingleField(tobj.fields, name, {
                get: () => tobj.functionPool[func + "_get"](currentelement, tobj),
                set: (v) => tobj.functionPool[func + "_set"](currentelement, v, tobj)
             });
          }
          else
          {
-            if(!property in currentelement)
+            if(!(property in currentelement))
                throw "No property " + property + " in template: " + tobj.name;
 
-            SingleFieldValue(tobj.fields, property, currentelement[property]);
+            SingleField(tobj.fields, name, {
+               get: () => currentelement[property], 
+               set: (v) => currentelement[property] = v 
+            });
          }
       }
       else
       {
          //It's the field!
-         SingleField(tobj.fields, value, {
-            get : () => currentelement.getAttribute("data-" + name),
-            set : (v) => currentelement.setAttribute("data-" + name, v)
+         SingleField(tobj.fields, name, {
+            get : () => currentelement.getAttribute(value),
+            set : (v) => currentelement.setAttribute(value, v)
          });
       }
    },
    //Standard initialization for my index templates, which recurses through
    //elemets to find t-variables 
-   StdTemplateScan: function(currentelement, tobj)
+   Scan: function(currentelement, tobj)
    {
       if(!currentelement)
          return;
 
       //Stop scanning if it was replaced, there's nothing left to do, DON'T
       //recurse through the newly replaced template (it alerady WAS)
-      if(StdTemplateTryReplace(currentelement, tobj))
+      if(TryReplace(currentelement, tobj))
          return;
 
       //Look for attributes and create standard redirects based on field value
       [...currentelement.attributes].forEach(x => 
       {
-         StdTemplateProcessField(currentelement, x.name, tobj);
+         ProcessField(currentelement, x.name, tobj);
       });
 
       //Call self for every child (recursive)
-      [...currentelement.children].forEach(x => StdTemplateScan(x, tobj));
+      [...currentelement.children].forEach(x => Scan(x, tobj));
    },
    //Get the template from the index, initializing all the inner crap etc. All
    //my templates should be this, you can add other templates ofc.
-   StdTemplate : function(template, functionPool)
+   Load : function(template, functionPool)
    {
-      var base = templates2.getElementById(template);
+      var base = templates2.content.getElementById(template);
       if(!base) throw "No template found with name: " + template;
       var element = base.cloneNode(true);
       var tobj = new Template(template, element, {});
       tobj.functionPool = functionPool;
       element.setAttribute("data-template", template);
       element.removeAttribute("id");
-      StdTemplateScan(element, tobj);
+      Scan(element, tobj);
       return tobj;
    }
-   //LoadTemplate : function(template, functionPool)
-   //{
-   //   return StdTemplate(template
-   //}
 })
 //Private vars can go here
 }(window));
 
+//And now, this is the "specific" template system...?
+var Templates = {
+   //appendclass_get : function(el)
+   //{
+   //   
+   //   var loglevel = "log_" + replace;
+   //   if(el.className.indexOf(loglevel) < 0)
+   //      el.className += " " + loglevel;
+   //   return loglevel;
+   //}
+   Load : function(template)
+   {
+      //Always try to load from ourselves first
+      var selfLoad = template + "_load";
+      if(selfLoad in Templates)
+         return Templates[selfLoad];
+      else
+         return StdTemplating.Load(template, Templates);
+   }
+};
